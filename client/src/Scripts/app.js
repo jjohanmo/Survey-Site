1aee7fb294c51083d40b1a10e1a4942d1ebfac7f
dishanktrivedi <dtrive26@my.centennialcollege.ca>
2021-03-21 16:10:55 +0530

    Contact page updated but not showing data

--- client/src/Scripts/app.js
+++ client/src/Scripts/app.js
@@ -6,3 +6,13 @@
     }
     window.addEventListener('load',Start);
 })();
+
+function results() 
+{
+    var fname = document.getElementById('fname').value;
+    var lname = document.getElementById('lname').value;
+    var subject = document.getElementById('subject').value;
+
+    alert( "You entered the following data: " +"\n\n"+"First Name: "+ fname + "\n\n" + "Last Name: "+lname  + "\n\n" +"Subject: " +phone+ "\n\n" + "Message: " +message);
+    window.location = '/contact';
+}
